74c7d70a56e61ee1defe15a90b1e66b1a7c79a24
NU-71163 <ciru22163@g.nihon-u.ac.jp>
2024-12-11 00:02:33 +0900

    編集

--- Assets/Script/ResultLoseAnimationMove.cs
+++ Assets/Script/ResultLoseAnimationMove.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class ResultLoseAnimationMove : MonoBehaviour
 {
     private Animator anim;
+    public AudioClip sound1;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void Lose()
     {
         anim.SetBool("Lose", true);
+    }
+
+    public void OnAnimationSound()
+    {
+        audioSource.PlayOneShot(sound1);
     }
 
     // Update is called once per frame
--- Assets/Script/ResultWinAnimationMove.cs
+++ Assets/Script/ResultWinAnimationMove.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class ResultWinAnimationMove : MonoBehaviour
 {
     private Animator anim;
+    public AudioClip sound1;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void Win()
     {
         anim.SetBool("Win", true);
+    }
+
+    public void OnAnimationSound()
+    {
+        audioSource.PlayOneShot(sound1);
     }
 
     // Update is called once per frame
